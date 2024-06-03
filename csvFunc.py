1ed816d279c09ddeeb3b311c541587a23c38c153
Yoselyn Miranda <91152700+Yoselyn214@users.noreply.github.com>
2024-06-03 17:13:40 -0500

    Update csvFunc.py

--- csvFunc.py
+++ csvFunc.py
@@ -1,11 +1,14 @@
 import pandas as pd
-import json
 
 def get_coordsCSV(city1):
-  data = pd.read_csv("worldcities.csv")
-  city = city1.city
-  country = city1.country
-  df = data.query("city == @city and country == @country")
-  df_json = str(df.to_json(orient='records', lines=True))
-  result = json.loads(df_json)
-  return result['lat'],result['lng']
+    data = pd.read_csv("worldcities.csv")
+    city = city1.city
+    country = city1.country
+    df = data[(data['city'] == city) & (data['country'] == country)]
+    if df.empty:
+        print(f"No se encontraron coordenadas para {city}, {country} en el archivo CSV.")
+        return None, None
+    else:
+        lat = df.iloc[0]['lat']
+        lng = df.iloc[0]['lng']
+        return float(lat), float(lng)
